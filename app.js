3cf84860acf6ef94952d240752491215b08db26a
Ashutosh Pawade <ashpawad@WKMZTE2DB86B.global.publicisgroupe.net>
2022-09-09 10:30:48 +0530

    Serving static html pages with css

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// app.use('/', (req, res, next) => {
-//     console.log('Into the first middleware');
-//     console.log(req.method, req.url);
-//     next();
-// });
-app.use('/users', (req, res) => {
-    console.log('Into the second middleware');
-    console.log('Second = ', req.method, req.url);
-    res.send('Hello Ashutosh!');
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/', (req, res, next) => {
-    console.log('Into the second middleware');
-    console.log('First = ', req.method, req.url);
-    res.send('Hello World!');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(2500, () => {
