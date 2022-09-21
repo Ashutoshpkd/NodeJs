df331244b13b3aaf184dc5825126ad7644882c19
Ashutosh Pawade <ashpawad@WKMZTE2DB86B.global.publicisgroupe.net>
2022-09-21 11:30:50 +0530

    Connecting to MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { body } = require('express-validator');
 const { feedRouter } = require('./routes/feed');
+const { join } = require('path');
 const port = 5050;
+const uri = `mongodb+srv://ashutoshpkd:naruto@rest-api.qmaic7m.mongodb.net/rest-api?retryWrites=true&w=majority`;
 
 const app = express();
 
@@ -10,13 +15,30 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Method', '*');
-    res.setHeader('Access-Control-Allow-Header', '*');
-
-    next();
-})
-
-app.use('/feed', feedRouter);
-
-app.listen(port, () => {
-    console.log(`SERVER UP AND RUNNING ON PORT - ... [${port}] ...`);
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    return next();
 });
+
+app.use('/feed', [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5}),
+], feedRouter);
+
+app.use('/images', express.static(join(__dirname, 'images')));
+
+app.use((error, req, res, next) => {
+  return res.status(error.statusCode).json({
+    message: error.message,
+  });
+});
+mongoose
+.connect(uri)
+  .then(() => {
+      console.log(`Connected to MongoDB - DB - ${process.env.MONGO_DB}`);
+      app.listen(port, () => {
+        console.log(`SERVER UP AND RUNNING ON PORT - ... [${port}] ...`);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- controller/feedController.js
+++ controller/feedController.js
@@ -1,16 +1,50 @@
+const { validationResult } = require('express-validator');
+
+const Post = require('../models/posts');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        title: "First Post!",
-        description: "This is an awesome post!",
-    });
+  res.status(200).json({
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/duck.jpg',
+        creator: {
+          name: 'Maximilian'
+        },
+        createdAt: new Date()
+      }
+    ]
+  });
 };
 
-exports.createPost = (req, res, next) => {
-    const body = req.body;
-    console.log(body);
-    res.status(201).json({
-        id: new Date().toISOString(),
-        message: 'Post created successfully!',
+exports.createPost = async (req, res, next) => {
+  const errors = validationResult(req);
+try {
+  if (!errors.isEmpty()) {
+    const err = new Error('Validation failed');
+    err.statusCode = 422;
+    throw err;
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/duck.jpg',
+    creator: { name: 'Ashutosh' }
+  });
+    const result = await post.save();
+    console.log('RAW = ', result);
+    return res.status(201).json({
+        message: 'Post created!',
+        post: result,
     });
+  } catch (err) {
+      if (!err.statusCode) {
+        err.statusCode = 422;
+      }
+      return next(err);
+  }
 };
